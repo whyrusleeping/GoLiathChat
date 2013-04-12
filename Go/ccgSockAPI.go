3a6f9d6f65c3dae65a157295edf82f7cc0c9333d
Jeromy <jeromyj@gmail.com>
2013-04-12 10:51:32 -0700

    tie in webkit launcher

--- Go/ccgSockAPI.go
+++ Go/ccgSockAPI.go
@@ -7,13 +7,18 @@ import (
 	"code.google.com/p/go.net/websocket"
 	"io/ioutil"
 	"os"
+	"time"
 	"strings"
+    "github.com/mattn/go-gtk/gtk"
+    "github.com/mattn/go-webkit/webkit"
 )
 
-var pathToIndex string
+var binDirectory string
 
 func httpHandler(c http.ResponseWriter, req *http.Request) {
-	index, _ := ioutil.ReadFile(pathToIndex)
+	file := binDirectory +  "../html" + req.URL.Path
+	log.Println(file)
+	index, _ := ioutil.ReadFile(file)
 	c.Write(index)
 }
 
@@ -100,7 +105,44 @@ func StartWebSockInterface() {
 	}
 }
 
+func StartWebkit() {
+	initPage("Goliath Chat", "http://127.0.0.1:8080/index.html", 600,600)
+}
+
+func initPage(title string, uri string, size_x int, size_y int) {
+	gtk.Init(nil)
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window.SetTitle(title)
+	window.Connect("destroy", gtk.MainQuit)
+
+	vbox := gtk.NewVBox(false, 1)
+
+	swin := gtk.NewScrolledWindow(nil, nil)
+	swin.SetPolicy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+	swin.SetShadowType(gtk.SHADOW_IN)
+
+	webview := webkit.NewWebView()
+	webview.LoadUri(uri)
+	swin.Add(webview)
+
+	vbox.Add(swin)
+
+	window.Add(vbox)
+	window.SetSizeRequest(size_x, size_y)
+	window.ShowAll()
+
+	proxy := os.Getenv("HTTP_PROXY")
+	if len(proxy) > 0 {
+		soup_uri := webkit.SoupUri(proxy)
+		webkit.GetDefaultSession().Set("proxy-uri", soup_uri)
+		soup_uri.Free()
+	}
+	gtk.Main()
+}
+
 func main() {
-	pathToIndex = strings.Replace(os.Args[0], "apicli", "index.html",1)
-	StartWebSockInterface()
+	binDirectory = strings.Replace(os.Args[0], "apicli", "",1)
+	go func() {StartWebSockInterface()}()
+	time.Sleep(time.Millisecond * 50)
+	StartWebkit()
 }
