49e48914d253cfabfb8d80aac9ce4e1bd0d8cec5
Jeromy <jeromyj@gmail.com>
2013-04-12 23:51:23 -0700

    add websocket to configure script

--- Go/configureAndBuild.sh
+++ Go/configureAndBuild.sh
@@ -2,5 +2,6 @@
 go get github.com/nsf/termbox-go
 go get code.google.com/p/go.crypto/scrypt
 go get github.com/toqueteos/webbrowser
+go get code.google.com/p/go.net/websocket
 make
 #cp bin/client ~/bin/gcChat
