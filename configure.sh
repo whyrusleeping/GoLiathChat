d31929bbaab7e52ad126207caa5c9997b53b63b7
Jeromy <jeromyj@gmail.com>
2013-09-25 18:58:23 +0000

    add some messages to the configure script

--- configure.sh
+++ configure.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+echo "Fetching Go Packages!"
+echo "getting nsf/termbox-go"
 go get github.com/nsf/termbox-go
+echo "getting go.crypto/scrypt"
 go get code.google.com/p/go.crypto/scrypt
+echo "getting toqueteos/webbrowser"
 go get github.com/toqueteos/webbrowser
+echo "getting go.net/websocket"
 go get code.google.com/p/go.net/websocket
+echo "getting nfnt/resize"
 go get github.com/nfnt/resize
